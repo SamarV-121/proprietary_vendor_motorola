ddbb51f72cbe5dad3e1d573a5227cafe1387ead5
SamarV-121 <samarvispute121@gmail.com>
2018-03-28 11:31:33 +0530

    Rename it to nicklaus

---
+++ nicklaus-vendor.mk
@@ -0,0 +1,2 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/nicklaus/vendor,system/vendor
