79ea209ca31c4c7fa130f91ecc1471c3e2e3b3a6
SamarV-121 <samarvispute121@gmail.com>
2018-03-28 11:23:50 +0530

    taido_row: update

--- taido_row/Android.mk
+++ taido_row/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter taido_row, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
 LOCAL_MODULE_OWNER := $VENDOR
@@ -9,3 +10,4 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_PREBUILT)
+endif
